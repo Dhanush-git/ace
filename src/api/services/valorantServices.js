24326004a1308232aa4464c041da302caa15b946
Dhanush <suvarnadhanush2000@gmail.com>
2022-04-25 01:27:39 +0530

    added get agent by id and get weapon functions

--- src/api/services/valorantServices.js
+++ src/api/services/valorantServices.js
@@ -1,7 +1,16 @@
 async function getAgents(){
     const data = await fetch('https://valorant-api.com/v1/agents').then(responce=> responce.json()).then(data=>data.data)
-    console.log(data)
     return data
 }
 
-module.exports = {getAgents}
+async function getAgentsByUuid(uuid){
+    const data = await fetch(`https://valorant-api.com/v1/agents/{uuid}`).then(responce=> responce.json()).then(data=>data.data)
+    return data
+}
+
+async function getWeapons(){
+    const data = await (await fetch('https://valorant-api.com/v1/weapons')).then(responce=> responce.json()).then(data=>data.data)
+    return data
+}
+
+module.exports = {getAgents,getAgentsByUuid,getWeapons}
