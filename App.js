95b849e46bf660146f59200bac7a946ef5ebe2a9
Dhanush <suvarnadhanush2000@gmail.com>
2022-04-25 01:27:57 +0530

    added loading screen

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoadingScreen from './components/screens/LoadingScreen';
 import { getAgents } from './src/api/services/valorantServices';
-
+import * as NavigationBar from 'expo-navigation-bar';
 export default function App() {
 
+  NavigationBar.setBackgroundColorAsync('black')
   getAgents()
 
   return (
     <View style={styles.container}>
-      <Text style={{fontWeight:'bold'}}>ACE</Text>
-      <StatusBar style="auto" />
+      <LoadingScreen/>
+      <StatusBar style="inverted" />
     </View>
   );
 }
---
+++ components/screens/LoadingScreen.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View,Image } from 'react-native'
+import React from 'react'
+import LottieView from 'lottie-react-native'
+
+const LoadingScreen = () => {
+  return (
+    <View style={LoadingStyles.container} >
+        <Image source={require('../../assets/logo.png')} style={{width:100,height:100,zIndex:1,transform:[{translateY:80}]}} />
+        <LottieView source={require('../../assets/lottie/loading_bar.json')} style={{width:200,zIndex:-1}} autoPlay loop/>
+    </View>
+  )
+}
+
+export default LoadingScreen
+
+const LoadingStyles = StyleSheet.create({
+    container:{
+        backgroundColor:'black',
+        flex:1,
+        width:'100%',
+        alignItems:'center',
+        justifyContent:'center'
+    }
+})
