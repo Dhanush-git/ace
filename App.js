c48d275b8cd85bf62a51afa3ee3f97d46cde35a4
Dhanush <suvarnadhanush2000@gmail.com>
2022-04-30 13:52:43 +0530

    updated WeaponsScreen

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import * as NavigationBar from 'expo-navigation-bar';
 import {useFonts,Oswald_700Bold} from '@expo-google-fonts/oswald'
 import WeaponInfoScreen from './components/screens/WeaponInfoScreen';
 import AgentsScreen from './components/screens/AgentsScreen';
+import WeaponsScreen from './components/screens/WeaponsScreen';
 
 
 export default function App() {
@@ -18,7 +19,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       {
-        isLoading || !fontsLoaded?<LoadingScreen/>:<AgentsScreen/>
+        isLoading || !fontsLoaded?<LoadingScreen/>:<WeaponsScreen/>
       }
       <StatusBar backgroundColor={'black'} barStyle={'light-content'}/>
     </View>
--- components/screens/WeaponsScreen.js
+++ components/screens/WeaponsScreen.js
@@ -1,10 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TouchableOpacity,Image} from 'react-native'
+import React,{useState,useEffect} from 'react'
+import { getWeapons } from '../../src/api/services/valorantServices'
+import LoadingScreen from './LoadingScreen'
 
 export default function WeaponsScreen() {
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [weapons, setWeapons] = useState()
+
+  useEffect(async() => {
+    const data = await getWeapons()
+    setWeapons(data)
+    setIsLoading(false)
+  }, [])
+  
   return (
-    <View>
-      <Text>WeaponsScreen</Text>
+    <View style={{flex:1,width:'100%',paddingHorizontal:20}}>
+      {isLoading?<LoadingScreen/>:
+      <View style={{flex:1,width:'100%'}}>
+        <Text style={{color:'white',fontFamily:'Oswald_700Bold',fontSize:30}}>WEAPONS</Text>
+        <ScrollView style={{marginTop:20}} showsVerticalScrollIndicator={false}>
+          {
+            weapons.map((e)=>
+              <TouchableOpacity key={e.displayName} style={{alignItems:'center',flexDirection:'row',height:80,width:'100%',borderRadius:10,backgroundColor:'white',marginBottom:10}}>
+                <Image source={{uri:e.displayIcon }} style={{height:80,width:80}} resizeMode='contain'/>
+                <View style={{marginLeft:20}}>
+                <Text style={{color:'black',fontSize:20,fontFamily:'Oswald_700Bold',letterSpacing:2}}>{e.displayName.toUpperCase()}</Text>
+                <Text >{e.category.split('::')[1]}</Text>
+                </View>
+              </TouchableOpacity>
+            )
+          }
+        </ScrollView>
+      </View>
+      }
     </View>
   )
 }
